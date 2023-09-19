8c6272b2e9b1a51e7420a13e6e938533965ea91f
Mikhalkevich <mikhalkevich@ya.ru>
2023-09-19 20:39:23 +0300

    rounded

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.base')
 @section('content')
     <h1 class="text-center text-3xl">{{$maintext->name}}</h1>
-    <div class="p-2 shadow-2xl border-r-2 m-2">
+    <div class="p-2 shadow-2xl border-r-2 m-2 rounded">
         {!! $maintext->body !!}
     </div>
 @endsection
